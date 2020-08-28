7f0263d61da9943ab9c5a5c60f4bdfafcef70573
veeralla-hyma <68535123+veeralla-hyma@users.noreply.github.com>
2020-08-28 18:54:26 +0530

    Update Hailstone.java

--- Hailstone.java
+++ Hailstone.java
@@ -10,11 +10,11 @@ public class Hailstone {
 		while(n>1) {
 		if(n%2==0) {
 			n=n/2;
-			System.out.println(n);
+			System.out.println(n+" is even so make it half : "n/2);
 		}
 		else {
 			n=(n*3)+1;
-			System.out.println(n);
+			System.out.println(n+"  is odd so i make 3n+1 : "+(3*n+1));
 		}
 		c++;
 	}
